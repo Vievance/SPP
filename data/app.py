a679024eae10d5f16a3f08bc39b1638646e418bb
Jacob Walker <jwalker@ABLAB.local>
2023-09-20 15:19:01 +0100

    ftse

--- data/app.py
+++ data/app.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
 import os 
+from ta import add_all_ta_features
+from ta.utils import dropna
+
 
 aapl = pd.read_csv("data/AAPL.csv")
 amzn = pd.read_csv("data/AMZN.csv")
@@ -46,20 +50,39 @@ stock_symbols = [
     "VOD",
     "ZM"
 ]
-for i in range(19):
-    stock = stocks[i]
-    stock['Date']= pd.to_datetime(stock['Date'])
+stock_data = []
+for symbol in stock_symbols:
+    file_path = os.path.join("data", f"{symbol}.csv")
+    stock_df = pd.read_csv(file_path)
+    stock_data.append(stock_df)
 
-    stock.set_index('Date', inplace=True)
+# Create a DataFrame for adjusted closing prices
+adj_close_df = pd.concat([df.set_index("Date")["Adj Close"].rename(symbol) for symbol, df in zip(stock_symbols, stock_data)], axis=1)
 
-    for ma in ma_day:
-        column_name = "MA for %s days" %(str(ma))
-        stock[column_name] = stock['Adj Close'].rolling(window=ma,center=False).mean()
-            
-    stock.tail()
+# Calculate the correlation matrix
+corr_matrix = adj_close_df.corr()
 
-    stock[['Adj Close','MA for 10 days','MA for 20 days','MA for 50 days']].plot(subplots=False,figsize=(12,5))
-    plt.xlabel("Date")
-    plt.ylabel('Price')
-    plt.title(f"{stock_symbols[i]} Moving Averages")
-    plt.savefig(f"{stock_symbols[i]}_ma.png")
+# Create a correlation heatmap
+plt.figure(figsize=(12, 10))
+sns.heatmap(corr_matrix, annot=True, cmap="coolwarm", linewidths=0.5)
+plt.title("Stock Price Correlation Heatmap")
+plt.show()
+
+def moving_average():
+    for i in range(19):
+        stock = stocks[i]
+        stock['Date']= pd.to_datetime(stock['Date'])
+
+        stock.set_index('Date', inplace=True)
+
+        for ma in ma_day:
+            column_name = "MA for %s days" %(str(ma))
+            stock[column_name] = stock['Adj Close'].rolling(window=ma,center=False).mean()
+                
+        stock.tail()
+
+        stock[['Adj Close','MA for 10 days','MA for 20 days','MA for 50 days']].plot(subplots=False,figsize=(12,5))
+        plt.xlabel("Date")
+        plt.ylabel('Price')
+        plt.title(f"{stock_symbols[i]} Moving Averages")
+        plt.savefig(f"{stock_symbols[i]}_ma.png")
